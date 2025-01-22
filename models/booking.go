151c5a0a8f29478cf1e0d5d56516f3f3fbd5223c
viniarifianir16 <viniarifianir16@gmail.com>
2025-01-23 01:25:20 +0700

    fix: fix type id

--- models/booking.go
+++ models/booking.go
@@ -4,8 +4,8 @@ import "time"
 
 type Booking struct {
 	ID         uint      `json:"id" gorm:"primaryKey;autoIncremet"`
-	CustomerID uint      `json:"customer_id" gorm:"unique" binding:"required"`
-	CarsID     uint      `json:"cars_id" gorm:"unique" binding:"required"`
+	CustomerID uint      `json:"customer_id" binding:"required"`
+	CarsID     uint      `json:"cars_id" binding:"required"`
 	StartRent  time.Time `json:"start_rent" gorm:"type:TIMESTAMP" binding:"required"`
 	EndRent    time.Time `json:"end_rent" gorm:"type:TIMESTAMP" binding:"required"`
 	TotalCost  uint      `json:"total_cost" binding:"required"`
